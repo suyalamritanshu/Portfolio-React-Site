fe425e5a54ad04f731ffc68c78fab166304bb5cc
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-11-08 23:56:07 +0530

    Home page ready

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import styled from "styled-components";
+import Sidebar from "./Components/Sidebar";
+import Home from "./Pages/Home";
+
+const App = () => {
+  // const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 1000);
+  // }, []);
+  return (
+    // <div className="App1">
+    //   {loading ? (
+    //     <ClimbingBoxLoader color={"#F3B042"} loading={loading} size={30} />
+    //   ) : (
+
+    <div className="App">
+      <Sidebar />
+      <MainContentStyled>
+        <div className="lines">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+          <div className="line-3"></div>
+          <div className="line-4"></div>
+        </div>
+
+        <Home />
+      </MainContentStyled>
+    </div>
+
+    // )}
+    // </div>
+  );
+};
+
+const MainContentStyled = styled.main`
+  position: relative;
+  margin-left: 16.3rem;
+  min-height: 100vh;
+  @media screen and (max-width:1200px){
+    margin-left: 0;
+  }
+  .lines{
+    position: absolute;
+    min-height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    opacity: 0.4;
+    z-index: -1;
+    .line-1, .line-2, .line-3, .line-4{
+      width: 1px;
+      min-height: 100vh;
+      background-color: var(--border-color);
+    }
+  }
+`;
+
+export default App;
